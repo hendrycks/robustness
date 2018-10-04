89bef2628f1066bc51d52420602af25aaf369e3f
nottombrown <nottombrown@gmail.com>
2018-10-04 16:17:07 -0700

    Update version

--- ImageNet-C/imagenet_c/setup.py
+++ ImageNet-C/imagenet_c/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imagenet_c",
-    version="0.0.1",
+    version="0.0.2",
     author="Dan Hendrycks",
     author_email="hendrycks@berkeley.edu",
     description="Access to ImageNet-C corruption functions",
